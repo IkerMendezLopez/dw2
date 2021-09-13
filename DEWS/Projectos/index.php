0f88132cebd0d4d90165a4750036c97aa1239518
mendez <ikermendezlopez@gmail.com>
2021-09-13 10:45:13 +0200

    cosas php

---
+++ DEWS/Projectos/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>TITULO</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <?php
+            echo "<a href='./01.php'>Ejercicio 1</a><br>";
+            echo "<a href='./02.php'>Ejercicio 2</a><br>";
+            echo "<a href='./03.php'>Ejercicio 3</a><br>";
+            echo "<a href='./04.php'>Ejercicio 4</a><br>";
+            echo "<a href='./05.php'>Ejercicio 5</a><br>";
+            echo "<a href='./06.php'>Ejercicio 6</a><br>";
+            echo "<a href='./07.php'>Ejercicio 7</a><br>";
+           ?>
+    </body>
+</html>
