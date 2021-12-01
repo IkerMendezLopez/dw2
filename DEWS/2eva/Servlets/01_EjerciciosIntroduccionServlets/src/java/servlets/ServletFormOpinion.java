6e18634a5289a4296862f39599f01bdfdf19784e
mendez <ikermendezlopez@gmail.com>
2021-12-01 12:00:24 +0100

    cosas de java y servlets

--- DEWS/2eva/Servlets/01_EjerciciosIntroduccionServlets/src/java/servlets/ServletFormOpinion.java
+++ DEWS/2eva/Servlets/01_EjerciciosIntroduccionServlets/src/java/servlets/ServletFormOpinion.java
@@ -5,8 +5,14 @@
  */
 package servlets;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,86 +24,84 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class ServletFormOpinion extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    private ArrayList<String> secciones;
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>FormOpinion</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Formulario Opinion</h1>");
-            out.println("<form method='get' action='ServletFormOpinion'>");
-            out.println("<label><b>Nombre:</b></label>");
-            out.println("<input type='text'><br><br>");
-            out.println("<label><b>Apellidos:</b></label>");
-            out.println("<input type='text'><br><br>");
-            out.println("<label><b>Opinion que le a merecido este sitio web</b></label><br>");
-            out.println("<input type='radio' value='B' name='opi'>");
-            out.println("<label>Buena</label><br>");
-            out.println("<input type='radio' value='R' name='opi'>");
-            out.println("<label>Regular</label><br>");
-            out.println("<input type='radio' value='M' name='opi'>");
-            out.println("<label>Mala</label><br><br>");
-            out.println("<label>Comentarios</label><br>");
-            out.println("<textarea rows='10' cols='40'></textarea><br><br>");
-            out.println("<label><b>Tus secciones favoritas</b></label><br><br>");
-            out.println("<input type='submit' value='Enviar Opinion'>");
-            out.println("</form>");
-            out.println("</body>");
-            out.println("</html>");
+            dibujarForm(out, false);
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        if (request.getParameter("nombre").equals("")) {
+            try (PrintWriter out = response.getWriter()) {
+                dibujarForm(out, true);
+            }
+        }
+        processRequest(request, response);
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    private void cargarSecciones(String file) {
+        secciones = new ArrayList<String>();
+        BufferedReader br;
+        try {
+            br = new BufferedReader(new FileReader(file));
+            String linea = br.readLine();
+            while (linea != null) {
+                secciones.add(linea);
+                linea = br.readLine();
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(ServletFormOpinion.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+    private void dibujarForm(PrintWriter out, boolean error) {
+        String required = "<span style='color:red;'>*</span>";
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>FormOpinion</title>");
+        out.println("</head>");
+        out.println("<body>");
+        if(error){
+            out.println("<p style='color:red;'>Completa todos los campos obligatorios("+required+")</p>");
+        }
+        out.println("<h1>Formulario Opinion</h1>");
+        out.println("<form method='post' action='ServletFormOpinion'>");
+        out.println("<label>"+required+"<b>Nombre:</b></label>");
+        out.println("<input type='text' name='nombre'><br><br>");
+        out.println("<label>"+required+"<b>Apellidos:</b></label>");
+        out.println("<input type='text' name='apellidos'><br><br>");
+        out.println("<label>"+required+"<b>Opinion que le a merecido este sitio web</b></label><br>");
+        out.println("<input type='radio' value='B' name='opi'>");
+        out.println("<label>Buena</label><br>");
+        out.println("<input type='radio' value='R' name='opi'>");
+        out.println("<label>Regular</label><br>");
+        out.println("<input type='radio' value='M' name='opi'>");
+        out.println("<label>Mala</label><br><br>");
+        out.println("<label>Comentarios</label><br>");
+        out.println("<textarea rows='10' cols='40' name='comentarios'></textarea><br><br>");
+        out.println("<label><b>Tus secciones favoritas</b></label><br><br>");
+        cargarSecciones(getServletContext().getRealPath("secciones.txt"));
+        for (String seccion : secciones) {
+            out.println("<input type='checkbox' name='secciones[]'>");
+            out.println("<label>" + seccion + "</label><br>");
+        }
+        out.println("<br><input type='submit' value='Enviar Opinion' name='enviar'>");
+        out.println("</form>");
+        out.println("</body>");
+        out.println("</html>");
+    }
 
 }
